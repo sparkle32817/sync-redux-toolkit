4fb1609024020db73c72e67fea73878f5f033901
Senior-Dev <sparkle172538@gmail.com>
2022-03-19 07:48:05 +0800

    add input for visuality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,40 @@
+import React from "react";
 import type { NextPage } from "next";
 import { useAppDispatch, useTypedSelector } from "../src/store";
-import { decrement, increment } from "../src/store/reducers/counterSlice";
+import { decrement, increment, changeString } from "../src/store/reducers/counterSlice";
 
 const Home: NextPage = () => {
-  const { value } = useTypedSelector((store) => store.counter);
+  const { value, string } = useTypedSelector((store) => store.counter);
   const dispatch = useAppDispatch();
 
   return (
     <div>
-      <span>{value}</span>
-      <button
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        Increase
-      </button>
-      <button
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      >
-        Decrease
-      </button>
+      <div>
+        <span>{value}</span>
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          Increase
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          Decrease
+        </button>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={string}
+          onChange={(e) => {
+            dispatch(changeString(e.target.value));
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/store/reducers/counterSlice.ts
+++ src/store/reducers/counterSlice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface CounterState {
   value: number;
+  string: string;
 }
 
 const initialState: CounterState = {
   value: 0,
+  string: "",
 };
 
 export const counterSlice = createSlice({
@@ -21,10 +23,13 @@ export const counterSlice = createSlice({
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
+    changeString: (state, action) => {
+      state.string = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, changeString } = counterSlice.actions;
 
 export default counterSlice.reducer;
